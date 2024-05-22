9e3b07aebe9f6a73092a61675919156f58c21d1b
ssun221 <157336321+ssun221@users.noreply.github.com>
2024-05-22 18:00:16 +0900

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,61 @@
+$(function(){
+    let ww=$(window).width();
+        
+    function fnSlider(item){
+        var swiper = new Swiper('.swiper',{                                   
+            effect:item,
+            loop:true,            
+            autoplay:{
+                delay:3000,
+            },
+            pagination:{
+                el:'.swiper-pagination',
+                clickable:true,
+            }, 
+        });
+    }
+    
+
+    if(ww>=768){
+        fnSlider('fade');
+    }else{
+        fnSlider('slide');
+    }
+    
+    function fnPopOpen(item){
+        $('.popup').eq(item).show();
+    }
+
+    $('.top03 .item').click(function(){
+        fnPopOpen($(this).index()-1);
+    })
+    $('.btn-close').click(function(){
+        $('.popup').hide();
+    })
+
+    let noticeList = $('.notice a');
+    $.ajax({
+        type:'GET',
+        url:`./json/notice-list.json`,
+        dataType:'json',
+
+        // 성공
+        success:function(data){ 
+            for(let idx=0;idx<=4;idx++){
+                noticeList.eq(idx).html(data[idx].noticeTitle);
+            }
+        },
+
+        // // 살패
+        // error:function(){
+        //     alert('죄송합니다. ㅠㅠ');                        
+        // },
+    })
+
+    noticeList.click(function(e){
+        let listNum = noticeList.index(this);
+        location.href=`./notice-view.html#${listNum}`;
+        e.preventDefault();
+    });
+
+})
